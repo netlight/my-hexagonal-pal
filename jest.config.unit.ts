5077f8c15cf810f13ab779a01a068194524e580c
Alexander Ungar <alexander.ungar@tum.de>
2024-03-09 13:34:48 +0100

    Remove Docker and GCP build files

--- jest.config.unit.ts
+++
@@ -1,9 +0,0 @@
-import baseConfig from "./jest.config"
-import { type Config } from "jest";
-
-const config: Config = {
-  ...baseConfig,
-  testRegex: ".*\\.unit\\.spec\\.ts$",
-}
-
-export default config;
